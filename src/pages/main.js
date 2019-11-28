df1b9e62248b308490950d546c94c1889fbfe239
Stan Boya <44618492+stanboya@users.noreply.github.com>
2019-11-27 22:11:56 -0800

    Centered the card and removed the backgrounds

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,11 @@
 /* eslint-disable */
 import React, { Component } from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Row, Col} from "reactstrap";
 import { Link, Route } from 'react-router-dom';
 import axios from "axios";
 import PropTypes from "prop-types";
 import Entry from "../components/form/entry-detail";
+import { cpus } from "os";
 
 class Main extends Component {
   constructor(props) {
@@ -27,28 +28,30 @@ class Main extends Component {
       entries = Object.keys(this.state).map((key) => {
         const url = "/entry_detail";
         return (
-          <div className="col-4">
-            <br/>
-            <Card key={key}>
-              <CardImg top height="300" src={Main.handleDisplayPicture(this.state[key].picture)} alt="Card image cap" />
-              <CardBody>
-              <CardTitle>{this.state[key].name}</CardTitle>
-              <CardSubtitle>{this.state[key].category}</CardSubtitle>
-              <CardText>{this.state[key].description}</CardText>
-              <Button>
-                <Link to={{ pathname: "/entry_detail", state: this.state[key] }} style={{ color: 'white' }}>Details</Link>
-              </Button>
-              </CardBody>
-            </Card>
-            <br/>
-          </div>
+            <Row className="mt-5">
+              <Col xxl="5" xl="4" lg="3" md="1" sm="1"></Col>
+              <Col xxl="2" xl="4" lg="6" md="10" sm="10">
+                <Card key={key}>
+                  <CardImg top height="300" src={Main.handleDisplayPicture(this.state[key].picture)} alt="Card image cap" />
+                  <CardBody>
+                  <CardTitle>{this.state[key].name}</CardTitle>
+                  <CardSubtitle>{this.state[key].category}</CardSubtitle>
+                  <CardText>{this.state[key].description}</CardText>
+                  <Button>
+                    <Link to={{ pathname: "/entry_detail", state: this.state[key] }} style={{ color: 'white' }}>Details</Link>
+                  </Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col xxl="5" xl="4" lg="3" md="1" sm="1"></Col>
+           </Row>
         );
       });
     }
 
     return (
       <div>
-        <h1 className="page_title">Active Posts</h1>
+        <h1 className="page_title mt-5">Active Posts</h1>
         {entries}
         <div />
       </div>
