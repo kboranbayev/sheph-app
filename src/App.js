1fa650253ac47089a21bb28020bca7f74c8cb30c
TacklessNebula3 <veerdhaliwal22@gmail.com>
2019-12-04 16:27:08 -0800

    - Dark mode implemented.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,56 @@ import Header from "./components/static/header.js";
 import Footer from "./components/static/footer.js";
 import Router from "./Router";
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    checked: localStorage.getItem("theme") === "dark",
+    theme: localStorage.getItem("theme")
+  };
+
+  componentDidMount() {
+    document
+      .getElementsByTagName("HTML")[0]
+      .setAttribute("data-theme", localStorage.getItem("theme"));
+  }
+
+  toggleThemeChange = () => {
+    const { checked } = this.state;
+    if (checked === false) {
+      localStorage.setItem("theme", "dark");
+      document
+        .getElementsByTagName("HTML")[0]
+        .setAttribute("data-theme", localStorage.getItem("theme"));
+      this.setState({
+        checked: true
+      });
+    } else {
+      localStorage.setItem("theme", "light");
+      document
+        .getElementsByTagName("HTML")[0]
+        .setAttribute("data-theme", localStorage.getItem("theme"));
+      this.setState({
+        checked: false
+      });
+    }
+  };
+
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <div className="App-theme">
           <Header backColor="#0062b3" />
-          <div className="main-content-container container-fluid px-4">
-            <Router />
+            <div className="main-content-container container-fluid px-4">
+              <Router /><br/>
+              <label className="switch">
+              <input
+                type="checkbox"
+                // checked={this.state.checked}
+                defaultChecked={this.state.checked}
+                onChange={() => this.toggleThemeChange()}
+              />
+              <span className="slider round"/>
+            </label>
+              <p className="text-monospace">Switch Mode</p>
           </div>
           <Footer />
         </div>
--- src/components/static/footer.js
+++ src/components/static/footer.js
@@ -5,12 +5,12 @@ class Footer extends React.Component {
     return (
     <footer className="footer text-center">
       <div className="container">
-        {/* Footer Bottom */}
+        {/* Footer Bottom */}<br/>
         <div className="footer-bottom">
           <p className="text-xs-center" style={{color: 'grey'}}>
             &copy;{new Date().getFullYear()} Sheph - All Rights Reserved |
             Contact us: <a href="mailto:sheph@gmail.com" target="_top" style={{color: '#55ACEE'}}>Sheph@gmail.com</a>
-          </p>
+          </p><br/>
         </div>
       </div>
     </footer>
