e6ff2b5d599360aa3b0f82cb507fc464dd60bff1
Alexander Song <alex.ky.song@gmail.com>
2019-12-05 15:02:03 -0800

    removed the Component definition, it was redundant thats why there was a warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/static/header.js";
